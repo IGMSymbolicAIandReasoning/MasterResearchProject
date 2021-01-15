ec1cabbdb3f290928bd2571cfc1f6a4f172c0262
JUILLARD <tjuillar@etud.u-pem.fr>
2021-01-15 13:35:51 +0100

    refactor :  refactoring production code

---
+++ src/main/scala/drugBankTD/LabelBase.scala
@@ -0,0 +1,5 @@
+package drugBankTD
+
+object LabelBase {
+  val INPUT = "file:///home/hoomar/Documents/m2/IA/ressources/lubm1.ttl"
+}
--- src/main/scala/drugBankTD/Main.scala
+++ src/main/scala/drugBankTD/Main.scala
@@ -3,7 +3,7 @@ package drugBankTD
 import drugBankTD.LubmExtractor
 
 object Main extends App {
-  val db = new LubmExtractor("file:C:\\Users\\pjgal\\OneDrive\\Bureau\\lubm1.ttl")
+  val db = new LubmExtractor(LabelBase.INPUT)
   db.load()
   // db.monSuperTest()
   // db.monPetitTest()
