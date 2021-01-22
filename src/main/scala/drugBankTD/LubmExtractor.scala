d28fb050bb58a59f381a598321d8c765d758ded5
JUILLARD <tjuillar@etud.u-pem.fr>
2021-01-22 22:52:20 +0100

    chore : divide the execution time by 12

--- src/main/scala/drugBankTD/LubmExtractor.scala
+++ src/main/scala/drugBankTD/LubmExtractor.scala
@@ -2,6 +2,7 @@ package drugBankTD
 
 import java.io.{FileWriter, IOException}
 import java.util
+import java.util.ArrayList
 import java.lang
 import java.util.Date
 import com.github.javafaker.Faker
@@ -10,7 +11,7 @@ import org.apache.jena.rdf.model.ModelFactory
 import java.text.SimpleDateFormat
 import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 
-class LubmExtractor(val dbSource: String, val male: Int, val vaccinationPercent: Int, vaccinesRepartition: util.ArrayList[Int], vaccines: util.ArrayList[String], val subjects :  util.ArrayList[String]) {
+class LubmExtractor(val dbSource: String, val male: Int, val vaccinationPercent: Int, vaccinesRepartition: ArrayList[Int], vaccines: ArrayList[String], val subjects :  ArrayList[String]) {
 
   vaccinesRepartition.forEach(e => if(e < 0 || e > 100) throw new IllegalArgumentException("Each element of vaccinesRepartition should be between 0 and 100"))
   vaccines.forEach(e => if(e.isEmpty) throw new IllegalArgumentException("Each element of vaccine should be non empty"))
@@ -31,115 +32,39 @@ class LubmExtractor(val dbSource: String, val male: Int, val vaccinationPercent:
   private val twenty : Date = sdf.parse("01/01/2001")
   private val thirty : Date = sdf.parse("01/01/1991")
   private val seventy : Date = sdf.parse("01/01/1951")
-  private val all_subjects = List[String]()
+  private val all_subjects = new ArrayList[String]()
 
 
 
+  /**
+   * Load data in model
+   */
   def load() = {
     model.read(dbSource, "TTL")
+    load_resources()
   }
 
-  def monSuperTest() = {
-    val iterator = model.listSubjects()
-    val res = scala.collection.mutable.Set[String]()
-    while (iterator.hasNext) {
-      res.add(iterator.next().getURI)
-    }
-    printf("result: " + res.toString())
-  }
-
-  def monGigaTest() = {
-    println("type property size " + model.listSubjectsWithProperty(rdfType).toList().size())
-    subjects.forEach(subject => {
-      val obj = model.createResource(subject)
-      val iterator = model.listSubjectsWithProperty(rdfType, obj)
-      var i = 0
-      while (iterator.hasNext()) {
-        iterator.next()
-        //printf(iterator.next().getURI.toString + "\n")
-        i = i + 1
-      }
-      println(subject + ": " + i)
-    })
-  }
 
   /**
-   * Display all people and the number of people
+   * create a list with all the subjects having the desired properties
    */
-  def monPetitTest() = {
-    println("type property size " + model.listSubjectsWithProperty(rdfType).toList().size())
+  def load_resources() = {
+    val subject_id : ArrayList[String] = subjects.foldLeft(new ArrayList[String]())((acc, s) => {
+      acc.add(s.substring(s.indexOf("#") + 1))
+      acc
+    })
     val iterator = model.listSubjectsWithProperty(rdfType)
-    var i = 0
     while (iterator.hasNext()) {
-      val uri = iterator.next().getURI.toString
-      val profession = uri.split("/")
-
-      if (profession.length == 4 && (uri.contains("AssistantProfessor") || uri.contains("AssociateProfessor") || uri.contains("FullProfessor") || uri.contains("GraduateStudent") || uri.contains("Lecturer") || uri.contains("UndergraduateStudent"))) {
-        i = i + 1
-        //println(uri)
+      val uri_done = iterator.next().getURI
+      val uri = uri_done.toString
+      val suject = uri.split("/")
+      if (suject.length == 4){
+        subject_id.forEach(x => {
+          if (uri.contains(x)) all_subjects.add(uri_done)
+        })
       }
     }
-    println("Tous les gens: " + i)
-  }
-
-
-  /**
-   * created random data based on subjectType, and class parameters
-   * @param subjectType URI represents a person owning a property on his professional situation
-   */
-  def extender() = {
-    subjects.forEach(subjectType => {
-      val iterator = model.listSubjectsWithProperty(rdfType, model.createResource(subjectType))
-      var low_birth_range = thirty
-      var hight_birth_range = seventy
-      if(! isOlder30(subjectType)){
-        low_birth_range = twenty
-        hight_birth_range = thirty
-      }
-
-      while (iterator.hasNext()) {
-        val faker = new Faker()
-        var uri = iterator.next().getURI
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#fName"), model.createLiteral(faker.name.firstName()))    // firstName
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#lName"), model.createLiteral(faker.name.lastName()))   //lastName
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#gender"), model.createResource("http://extension.group1.fr/onto#" + randomGender()))   //Genre
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#zipcode"), model.createLiteral(faker.address.zipCode()))   //ZipCode
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#birhtdate"), model.createLiteral(faker.date().between(hight_birth_range, low_birth_range).toString))
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#state"), model.createLiteral(faker.address.state())) // Address
-        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#id"), model.createLiteral(faker.idNumber().toString)) // ID
-      }
-    })
-
-  }
-
-  /**
-   * adds properties of vaccines to some person based on vaccinationPercent
-   * @param subjectType URI represents a person owning a property on his professional situation
-   */
-  def extender_vaccine() = {
-    subjects.forEach(subjectType => {
-      val iterator = model.listSubjectsWithProperty(rdfType, model.createResource(subjectType))
-      while (iterator.hasNext()) {
-        val faker = new Faker()
-        var uri = iterator.next().getURI
-        if (randomVaccinator()) model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#vaccine"), model.createResource("http://extension.group1.fr/onto#" + randomVaccine()))
-      }
-    })
-  }
-
-  /**
-   * TODO changer ça et faire en sorte qu'on conaisse les ages grçace au fichier de config
-   * detects the age range of the person based on URI
-   * @param subjectType URI represents a person owning a property on his professional situation
-   * @return
-   */
-  private def isOlder30(subjectType: String) : Boolean = {
-    val position = subjectType.indexOf("#")
-    val profession = subjectType.substring(position)
-    if (profession.equals("#AssistantProfessor") || profession.equals("#AssociateProfessor") || profession.equals("#FullProfessor") || profession.equals("#Lecturer")) {
-      return true
-    }
-    return false
+    print("They are " + all_subjects.size + " subjects with the desired properties. ")
   }
 
   /**
@@ -186,5 +111,116 @@ class LubmExtractor(val dbSource: String, val male: Int, val vaccinationPercent:
       case closeException: IOException =>
       // ignore
     }
+  }
+
+  /**
+   * detects the age range of the person based on URI
+   * @param subjectType URI represents a person owning a property on his professional situation
+   * @return
+   */
+  private def isOlder30(subjectType: String) : Boolean = {
+    if (subjectType.contains("AssistantProfessor") || subjectType.contains("AssociateProfessor") || subjectType.contains("FullProfessor") || subjectType.contains("Lecturer")) {
+      return true
+    }
+    return false
+  }
+
+  /**
+   * created random data based on subjectType, and class parameters
+   * @param subjectType URI represents a person owning a property on his professional situation
+   */
+  def extender() = {
+    val faker = new Faker()
+    all_subjects.forEach(uri => {
+      var low_birth_range = thirty
+      var hight_birth_range = seventy
+      if(! isOlder30(uri)) {
+        low_birth_range = twenty
+        hight_birth_range = thirty
+      }
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#fName"), model.createLiteral(faker.name.firstName()))    // firstName
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#lName"), model.createLiteral(faker.name.lastName()))   //lastName
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#gender"), model.createResource("http://extension.group1.fr/onto#" + randomGender()))   //Genre
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#zipcode"), model.createLiteral(faker.address.zipCode()))   //ZipCode
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#birhtdate"), model.createLiteral(faker.date().between(hight_birth_range, low_birth_range).toString))
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#state"), model.createLiteral(faker.address.state())) // Address
+      model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#id"), model.createLiteral(faker.idNumber().toString)) // ID
+    })
+  }
+
+  /**
+   * adds properties of vaccines to some person based on vaccinationPercent
+   * @param subjectType URI represents a person owning a property on his professional situation
+   */
+  def extender_vaccine() = {
+    val faker = new Faker()
+    all_subjects.forEach(uri => {
+      if (randomVaccinator()) model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#vaccine"), model.createResource("http://extension.group1.fr/onto#" + randomVaccine()))
+    })
+  }
+
+
+
+  /**************************************************************************
+   *                                 OLD                                    *
+   **************************************************************************
+   */
+
+  /**
+   * created random data based on subjectType, and class parameters
+   * @param subjectType URI represents a person owning a property on his professional situation
+   */
+  def old_extender() = {
+    subjects.forEach(subjectType => {
+      val iterator = model.listSubjectsWithProperty(rdfType, model.createResource(subjectType))
+      var low_birth_range = thirty
+      var hight_birth_range = seventy
+      if(! old_isOlder30(subjectType)){
+        low_birth_range = twenty
+        hight_birth_range = thirty
+      }
+
+      while (iterator.hasNext()) {
+        val faker = new Faker()
+        var uri = iterator.next().getURI
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#fName"), model.createLiteral(faker.name.firstName()))    // firstName
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#lName"), model.createLiteral(faker.name.lastName()))   //lastName
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#gender"), model.createResource("http://extension.group1.fr/onto#" + randomGender()))   //Genre
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#zipcode"), model.createLiteral(faker.address.zipCode()))   //ZipCode
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#birhtdate"), model.createLiteral(faker.date().between(hight_birth_range, low_birth_range).toString))
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#state"), model.createLiteral(faker.address.state())) // Address
+        model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#id"), model.createLiteral(faker.idNumber().toString)) // ID
+      }
+    })
+  }
+
+  /**
+   * adds properties of vaccines to some person based on vaccinationPercent
+   * @param subjectType URI represents a person owning a property on his professional situation
+   */
+  def old_extender_vaccine() = {
+    subjects.forEach(subjectType => {
+      val iterator = model.listSubjectsWithProperty(rdfType, model.createResource(subjectType))
+      while (iterator.hasNext()) {
+        val faker = new Faker()
+        var uri = iterator.next().getURI
+        if (randomVaccinator()) model.add(model.createResource(uri), model.createProperty("http://extension.group1.fr/onto#vaccine"), model.createResource("http://extension.group1.fr/onto#" + randomVaccine()))
+      }
+    })
+  }
+
+  /**
+   * TODO changer ça et faire en sorte qu'on conaisse les ages grçace au fichier de config
+   * detects the age range of the person based on URI
+   * @param subjectType URI represents a person owning a property on his professional situation
+   * @return
+   */
+  private def old_isOlder30(subjectType: String) : Boolean = {
+    val position = subjectType.indexOf("#")
+    val profession = subjectType.substring(position)
+    if (profession.equals("#AssistantProfessor") || profession.equals("#AssociateProfessor") || profession.equals("#FullProfessor") || profession.equals("#Lecturer")) {
+      return true
+    }
+    return false
   }
 }
--- src/main/scala/drugBankTD/Main.scala
+++ src/main/scala/drugBankTD/Main.scala
@@ -5,23 +5,34 @@ import java.util
 
 
 object Main extends App {
+  /*
+   * Multiple call listSubjectsWithProperty
+   * Elapsed time: 75 608 855 521ns
+   *
+   * One call listSubjectsWithProperty
+   * Elapsed time: 6 828 382 394ns
+   */
+
+  val t0 = System.nanoTime()
 
   val db = new LubmExtractor(LabelBase.INPUT_FILE, LabelBase.MALE_PERCENT, LabelBase.PEOPLE_VACCINATED_PERCENT, LabelBase.VACINES_PERCENT, LabelBase.VACCINES_NAMES, LabelBase.SUJECTS)
 
-
   db.load()
 
-  db.monGigaTest()
-
-  db.monPetitTest()
-
-
+  print("creates data : ")
   db.extender()
-  println("extender : done")
+  println("done.")
+
+  print("creates vaccine data : ")
   db.extender_vaccine()
-  println("extender vaccine : done")
+  println("done.")
+
+  print("writes in file : ")
   db.toFile(LabelBase.OUTPUT_FILE)
-  println("write file : done")
+  println("done")
+
+   val t1 = System.nanoTime()
+   println("Elapsed time: " + (t1 - t0) + "ns")
 
 }
 
