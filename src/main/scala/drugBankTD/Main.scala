ee9d661e3fe82b4b846428124407f4a4ac546662
JUILLARD <tjuillar@etud.u-pem.fr>
2021-01-24 18:27:47 +0100

    chore : put away the code

--- src/main/scala/drugBankTD/Main.scala
+++ src/main/scala/drugBankTD/Main.scala
@@ -5,15 +5,6 @@ import java.util
 
 
 object Main extends App {
-  /*
-   * Multiple call listSubjectsWithProperty
-   * Elapsed time: 75 608 855 521ns
-   *
-   * One call listSubjectsWithProperty
-   * Elapsed time: 6 828 382 394ns
-   */
-
-  val t0 = System.nanoTime()
 
   val db = new LubmExtractor(LabelBase.INPUT_FILE, LabelBase.MALE_PERCENT, LabelBase.PEOPLE_VACCINATED_PERCENT, LabelBase.VACINES_PERCENT, LabelBase.VACCINES_NAMES, LabelBase.SUJECTS)
 
@@ -30,8 +21,5 @@ object Main extends App {
   print("writes in file : ")
   db.toFile(LabelBase.OUTPUT_FILE)
   println("done")
-
-   val t1 = System.nanoTime()
-   println("Elapsed time: " + (t1 - t0) + "ns")
 }
 
